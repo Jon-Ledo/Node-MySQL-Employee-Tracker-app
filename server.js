ddca19b69f6292975030203fea0567399a1ad8e9
JonLedo <jonledo.code@gmail.com>
2022-11-23 16:12:09 -0500

    cleaned up some code, moved/added comments. created an exit process to shut down app when finished. combined some lines in server.js as well and included a generic 404 msg

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 
-app.listen(PORT, console.log(`Server is listening on port ${PORT}...`))
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end()
+})
 
-askPrompt()
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`)
+  askPrompt()
+})
