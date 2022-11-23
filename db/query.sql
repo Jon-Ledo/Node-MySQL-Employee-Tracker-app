197da2c0a02554ce4975704b1c03864f197dbd75
JonLedo <jonledo.code@gmail.com>
2022-11-23 15:01:01 -0500

    updated SQL command to fetch all relevant employee data for the getEmployees function.

--- db/query.sql
+++ db/query.sql
@@ -19,3 +19,18 @@ SELECT emp.id, emp.first_name, emp.last_name, emp.manager_id, mngr.first_name AS
 FROM employee AS emp
 LEFT JOIN employee AS mngr
 ON emp.manager_id = mngr.id;
+
+-- show all relevant employee data
+SELECT 	employee.id, 
+		    employee.first_name, 
+        employee.last_name, 
+        role.title AS title,
+        role.salary AS salary,
+        department.name AS department,
+        CONCAT (manager.first_name, " ", manager.last_name) AS manager 
+FROM employee
+LEFT JOIN role
+ON employee.role_id = role.id
+LEFT JOIN department 
+ON role.department_id = department.id
+LEFT JOIN employee manager ON employee.manager_id = manager.id;
